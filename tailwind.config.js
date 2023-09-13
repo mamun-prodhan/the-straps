2b60ab3bcdb0576b9663959d4263892561cbc57b
mamun-prodhan <mamunpr9@gmail.com>
2023-09-13 14:01:10 +0600

    final commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        paint: "url('./src/assets/bg.png')",
+        paint: "url('../src/assets/bg.png')",
       },
     },
   },
